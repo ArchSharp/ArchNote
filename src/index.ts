abe98aadd9afe4e48413a847e9a3a694f83fe5b5
ArchSharp <raphael.tunji@yahoo.com>
2023-07-26 15:56:57 +0100

    Using cluster to create many app instances

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import http from "http";
+import cluster from "cluster";
+import { cpus } from "os";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
@@ -19,60 +21,73 @@ import * as fs from "fs";
 // const producer = new Producer();
 // const consumer = new Consumer();
 
-const app = express();
+if (cluster.isPrimary) {
+  // Fork workers equal to the number of CPU cores
+  const numCPUs = cpus().length;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  console.log("Number of CPU(s): ", numCPUs);
 
-app.use(cors({ credentials: true }));
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use("/anote", anoteRoute);
-app.use("/school", schoolRoute);
-app.use("/staff", staffRoute);
-app.use("/payment", paymentRoute);
-app.use("/httpclient", httpRoute);
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  const app = express();
 
-app.use("/generateinvoice", (req: Request, res: Response) => {});
+  app.use(cors({ credentials: true }));
+  app.use(compression());
+  app.use(cookieParser());
+  app.use(bodyParser.json());
+  app.use("/anote", anoteRoute);
+  app.use("/school", schoolRoute);
+  app.use("/staff", staffRoute);
+  app.use("/payment", paymentRoute);
+  app.use("/httpclient", httpRoute);
 
-app.use("/convertfromoffice", async (req: Request, res: Response) => {
-  const { filename } = req.query;
+  app.use("/generateinvoice", (req: Request, res: Response) => {});
 
-  const inputPath = path.resolve(__dirname, `./Files/${filename}.docx`);
-  const outputPath = path.resolve(__dirname, `./Files/${filename}.pdf`);
+  app.use("/convertfromoffice", async (req: Request, res: Response) => {
+    const { filename } = req.query;
 
-  const convertToPdf = async () => {
-    const pdfdoc = await PDFNet.PDFDoc.create();
-    await pdfdoc.initSecurityHandler();
-    await PDFNet.Convert.toPdf(pdfdoc, inputPath);
-    pdfdoc.save(outputPath, PDFNet.SDFDoc.SaveOptions.e_linearized);
-  };
+    const inputPath = path.resolve(__dirname, `./Files/${filename}.docx`);
+    const outputPath = path.resolve(__dirname, `./Files/${filename}.pdf`);
 
-  await PDFNet.runWithCleanup(
-    convertToPdf,
-    "demo:1687870598757:7d91b63b03000000009ef8ab7bef0993b032e25c1e23d8c1baf5355978"
-  )
-    .then(() => {
-      fs.readFile(outputPath, (err, data) => {
-        if (err) {
-          res.status(500).end(err);
-        } else {
-          res.setHeader("ContentType", "application/pdf");
-          res.sendFile(outputPath);
-          //  .send(data);
-        }
+    const convertToPdf = async () => {
+      const pdfdoc = await PDFNet.PDFDoc.create();
+      await pdfdoc.initSecurityHandler();
+      await PDFNet.Convert.toPdf(pdfdoc, inputPath);
+      pdfdoc.save(outputPath, PDFNet.SDFDoc.SaveOptions.e_linearized);
+    };
+
+    await PDFNet.runWithCleanup(
+      convertToPdf,
+      "demo:1687870598757:7d91b63b03000000009ef8ab7bef0993b032e25c1e23d8c1baf5355978"
+    )
+      .then(() => {
+        fs.readFile(outputPath, (err, data) => {
+          if (err) {
+            res.status(500).end(err);
+          } else {
+            res.setHeader("ContentType", "application/pdf");
+            res.sendFile(outputPath);
+            //  .send(data);
+          }
+        });
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.end(err);
       });
-    })
-    .catch((err) => {
-      res.statusCode = 500;
-      res.end(err);
-    });
-});
+  });
 
-const server = http.createServer(app);
+  const server = http.createServer(app);
 
-const ip = "127.0.0.1";
-// const ip = "192.168.193.174";
-const port = 5000;
+  //const ip = "127.0.0.1";
+  const ip = "192.168.137.1";
+  const port = 5000;
 
-server.listen(port, ip, () => {
-  console.log(`Server running on http://${ip}:${port}/`);
-});
+  server.listen(port, ip, () => {
+    console.log(`Server running on http://${ip}:${port}/`);
+  });
+}
